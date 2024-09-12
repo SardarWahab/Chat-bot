28e4ae50b2f8d6ba02e0e3906f0a765b7197cedb
SardarWahab <wahabsardar130@gmail.com>
2024-09-12 23:36:08 +0500

    mycom

---
+++ myChatbot/myChatbot/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib import messages
+
+def home(request):
+    return render(request, 'index.html')
+from .chatbot import get_ai_answer
+def chatbot(request):
+    if request. method == 'POST':
+        question = request.POST.get("message")
+        answer = get_ai_answer(question)
+        messages.info(request,answer)
+
+      
+    
+    return redirect('home')
