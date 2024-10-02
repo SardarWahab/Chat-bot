145af5bf47603f6846b520bd3a0485c5cc6d83e0
SardarWahab <wahabsardar130@gmail.com>
2024-10-02 12:02:39 +0500

    mycom

--- myChatbot/myChatbot/urls.py
+++ myChatbot/myChatbot/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
     path('',home ,name='home'),
     path('chat/',chatbot, name='chatbot')
 ]
+
--- myChatbot/myChatbot/views.py
+++ myChatbot/myChatbot/views.py
@@ -6,7 +6,7 @@ def home(request):
 
 from .chatbot import get_ai_answer
 def chatbot(request):
-    if request. method == 'POST':
+    if request.method == 'POST':
         question = request.POST.get("message")
         answer = get_ai_answer(question)
         messages.info(request,answer)
